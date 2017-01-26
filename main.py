155cd5137279e4c9ac5a48b71f5b4b36b12fcd73
Kyle Mann <akavir7@gmail.com>
2017-01-26 14:32:34 -0600

    add encryption and submit button

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 #
 import webapp2
+import caesar
 import helpers
+
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        message = "Hellooooo world!"
+        encrypted_message = caesar.encrypt(message, 13)
+        
+        textarea = "<textarea>" + encrypted_message + "</textarea>"
+        submit = "<input type='submit'/>"
+        form = "<form>" + textarea + "</form>" + "<br>" + submit
+        
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
